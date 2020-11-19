25beff6b17d94e2c106471c6daaf66f2cb50c71d
kwuzy <kevinlwu168@gmail.com>
2020-11-19 12:18:31 -0800

    need to fix animation for action idle

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -19,6 +19,8 @@ public class PlayerController : MonoBehaviour
 
         playerRB.velocity = new Vector2(horizontalMove,veritcalMove) * moveSpeed;
         Vector2 actionVector = new Vector2(action.Horizontal,action.Vertical);
+
+        //moving animation
         if(actionVector == Vector2.zero) {
             playerAnim.SetFloat("moveX", playerRB.velocity.x);
             playerAnim.SetFloat("moveY", playerRB.velocity.y);
@@ -26,6 +28,17 @@ public class PlayerController : MonoBehaviour
             playerAnim.SetFloat("moveX", actionVector.x);
             playerAnim.SetFloat("moveY", actionVector.y);
         }
-        
+
+        //idle animation
+        if (horizontalMove > 0.1 || horizontalMove < -0.1 || veritcalMove > 0.1 || veritcalMove < -0.1) {
+            if(action.Horizontal > 0.1 || action.Horizontal < -0.1 || action.Vertical > 0.1 || action.Vertical < -0.1) {
+                playerAnim.SetFloat("lastX",action.Horizontal);
+                playerAnim.SetFloat("lastY",action.Vertical);
+            } else {
+                playerAnim.SetFloat("lastX",horizontalMove);
+                playerAnim.SetFloat("lastY",veritcalMove);
+            }
+        }
+
     }
 }
