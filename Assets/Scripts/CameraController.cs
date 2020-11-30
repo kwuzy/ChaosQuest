8a8503a7bd950821534517f7d37ecf30d967854a
kwuzy <30635580+kwuzy@users.noreply.github.com>
2020-11-30 00:04:43 -0500

    starting camera work

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,19 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class CameraController : MonoBehaviour
 {
     public Transform target;
-    
-    void Start()
-    {
-        
+    public Tilemap theMap;
+    private Vector3 bottomLeftLimit;
+    private Vector3 topRightLimit;
+    private float halfHeight;
+    private float halfWidth;
+
+    void Start() {
+        target = PlayerController.instance.transform;
+        //target = FindObjectOfType<PlayerController>().transform;
+
+        halfHeight = Camera.main.orthographicSize;
+        halfWidth = halfHeight * Camera.main.aspect;
+
+        bottomLeftLimit = theMap.localBounds.min + new Vector3(halfWidth, halfHeight, 0f);
+        topRightLimit = theMap.localBounds.max - new Vector3(halfWidth, halfHeight - 0.9f, 0f);
+
+        PlayerController.instance.SetBounds(theMap.localBounds.min, theMap.localBounds.max);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void LateUpdate() {
+        transform.position = new Vector3(target.position.x,target.position.y,transform.position.z);
+
+        //keep the camera inside the bounds
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, bottomLeftLimit.x, topRightLimit.x), Mathf.Clamp(transform.position.y, bottomLeftLimit.y, topRightLimit.y), transform.position.z);
     }
 }
